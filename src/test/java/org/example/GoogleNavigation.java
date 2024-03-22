62d39740e9b7e35eadf5834b25beb61647d7c8b8
chathurahjm <chathudagr8@yahoo.com>
2024-03-22 16:48:36 +0530

    g

--- src/test/java/org/example/GoogleNavigation.java
+++ src/test/java/org/example/GoogleNavigation.java
@@ -19,7 +19,7 @@ public class GoogleNavigation {
     protected WebDriver driver;
     
    
-    //@Test
+    @Test
     public void testActions()throws InterruptedException {
 
            ExecutorService executorService = Executors.newFixedThreadPool(15);
@@ -40,56 +40,17 @@ public class GoogleNavigation {
                         // Create a WebDriver instance (ChromeDriver)
                         WebDriver driver = new ChromeDriver(chromeOptions);
 
-                       
+                        // Navigate to Google
                         driver.get("https://www.youtube.com/@day2day/playlists");
 
                         Thread.sleep(8000); // Sleep
                         WebElement element = driver.findElement(By.xpath("(//*[@class='yt-simple-endpoint style-scope ytd-playlist-thumbnail'])[4]"));
 
-                        
+                        // Simulate pressing the space button on the element
                         element.click();
 
                         // Sleep for 15 minutes
                         Thread.sleep(TimeUnit.MINUTES.toMillis(15));
-
-                        driver.quit();
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-
-        // Shutdown the executor once all tasks are completed
-        executorService.shutdown();
-        
-    }
-
-       @Test
-    public void testActions2()throws InterruptedException {
-
-           ExecutorService executorService = Executors.newFixedThreadPool(15);
-
-        // Submit tasks to the executor
-        for (int i = 0; i < 15; i++) {
-            executorService.submit(() -> {
-                try {
-                    long startTime = System.currentTimeMillis();
-                    while (System.currentTimeMillis() - startTime < TimeUnit.HOURS.toMillis(17)) {
-                        // Set ChromeDriver path
-                        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-
-                        // Configure ChromeOptions
-                        ChromeOptions chromeOptions = new ChromeOptions();
-                        chromeOptions.addArguments("--headless");
-
-                        // Create a WebDriver instance (ChromeDriver)
-                        WebDriver driver = new ChromeDriver(chromeOptions);
-
-                       
-                        driver.get("https://www.gplus.lk");
-
-                        Thread.sleep(8000); // Sleep
 
                         driver.quit();
                     }
